af1d5d22d615fa25086d48c0ac7405cb5d480816
gaurav1Nn <gauravnilawar26@gmail.com>
2024-12-20 21:26:11 +0530

    Time: 3 ms (64.48%), Space: 24.4 MB (6.19%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,16 +1,24 @@
 class MinStack {
 public:
+
     stack<int> st;
+    stack<int> helper;
     MinStack() {
-       
+        
     }
     
     void push(int val) {
         st.push(val);
+        if(helper.size()==0 || val<helper.top()){
+            helper.push(val);
+            
+        }
+        else helper.push(helper.top());
     }
     
     void pop() {
         st.pop();
+        helper.pop();
     }
     
     int top() {
@@ -18,18 +26,7 @@ public:
     }
     
     int getMin() {
-        stack<int> helper;
-        int mn = INT_MAX;
-        while(st.size()>0){
-            mn = min(mn,st.top());
-            helper.push(st.top());
-            st.pop();
-        }
-        while(helper.size()>0){
-            st.push(helper.top());
-            helper.pop();
-        }
-        return mn;
+        return helper.top();
     }
 };
 
