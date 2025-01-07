43f8a7d146f88d974e3fb981ed931ef2c60d4ee5
gaurav1Nn <gauravnilawar26@gmail.com>
2025-01-07 23:07:50 +0530

    Time: 0 ms (100%), Space: 25.3 MB (71.67%) - LeetHub

--- 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.cpp
+++ 2847-find-maximum-number-of-string-pairs/2847-find-maximum-number-of-string-pairs.cpp
@@ -5,17 +5,16 @@ public:
         unordered_set<string> s;
         int count =0;
         for(int i=0;i<n;i++){
-            s.insert(arr[i]);
-        }
-        for(int i=0;i<n;i++){
             string rev = arr[i];
             reverse(rev.begin(),rev.end());
-            if(arr[i]==rev) continue;
             if(s.find(rev)!=s.end()){
                 count++;
-                s.erase(arr[i]);
+            }
+            else {
+                s.insert(arr[i]);
             }
         }
+       
         return count;
     }
 };
