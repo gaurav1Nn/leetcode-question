67b0ce1a75f972464212a3d48ae60f9c851631de
gaurav1Nn <gauravnilawar26@gmail.com>
2025-01-10 20:20:28 +0530

    Time: 0 ms (100%), Space: 15.9 MB (23.79%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,26 +1,45 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        // stl function  next_permutation(nums.begin(),nums.end)
-        int idx = -1;
-        int n = nums.size();
-        for(int i=n-2;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                idx = i ; 
+    void nextPermutation(vector<int>& arr) {
+        int index = -1;
+        int n = arr.size();
+
+        //step1:
+        //find any index (peak) where arr[i]<arr[i+1] and break whenever you get that
+
+            for(int i=n-2; i>=0; i--){
+            if(arr[i]<arr[i+1]) {
+                index = i;
                 break;
             }
         }
-        if(idx==-1){
-            reverse(nums.begin(),nums.end());
+
+        // for the edge case of last element's permutataion
+        //if we are unable to find any index such that arr[i]<arr[i+1] this means we are at the last permutation
+        //answer will be the very first permutation that would be reverse of last one
+
+        if(index==-1) {
+            reverse(arr.begin(),arr.end());
             return;
         }
-        for(int i = n-1;i>idx;i--){
-            if(nums[i]>nums[idx]){
-                swap(nums[i],nums[idx]);
+
+        // step 2:
+        // find out the next greater element at right than arr[index]
+        //traversing from right would be beneficial
+        // after getting that element swap it with the arr[index]
+        for(int i=n-1; i>=0; i--){
+            if(arr[i]>arr[index]) {
+                swap(arr[index],arr[i]) ;
                 break;
-            }
+            } 
         }
-      reverse(nums.begin()+idx+1,nums.end());
-    //   return nums;
+   
+        // remaining answer will be decreasingly sorted automatically
+        //just reverse or sort 
+        
+        reverse(arr.begin()+index+1, arr.end());
+
+
+
     }
 };
