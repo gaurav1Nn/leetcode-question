6ec61a8b2ca8a41ae2fe249f82f05645156b5f7a
gaurav1Nn <gauravnilawar26@gmail.com>
2025-01-21 19:41:35 +0530

    Time: 295 ms (5.12%), Space: 174 MB (20.18%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
-        int rows=mat.size();
-        int col=mat[0].size();
-        map<int,int>mpr;
-        map<int,int>mpcol;
-        map<int,pair<int,int>>mpmat;
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<col; j++){
-                mpmat[mat[i][j]]={i,j};
+        map<int,int> mapr;
+        map<int,int> mapc;
+        int rows = mat.size();
+        int col = mat[0].size();
+        map<int,pair<int,int>> matp;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<col;j++){
+                matp[mat[i][j]] = {i,j};
             }
         }
-        int ans=0;
-        for(int i=0; i<arr.size(); i++){
-            int r=mpmat[arr[i]].first;
-            int c=mpmat[arr[i]].second;
-            mpr[r]++;
-            mpcol[c]++;
-            if(mpr[r]==col || mpcol[c]>=rows){
-                ans=i;
-                break;
+        int ans = 0;
+        for(int i=0;i<arr.size();i++){
+            int r = matp[arr[i]].first;
+            int c = matp[arr[i]].second;
+            mapr[r]++;
+            mapc[c]++;
+            if(mapr[r]==col || mapc[c]==rows){
+                ans = i;
+                return ans;
             }
-
         }
         return ans;
-
-
     }
 };
