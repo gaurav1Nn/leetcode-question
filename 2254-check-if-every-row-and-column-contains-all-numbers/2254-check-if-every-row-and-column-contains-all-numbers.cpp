8cefae15a75631efa021a62195739350ea6b8392
gaurav1Nn <gauravnilawar26@gmail.com>
2025-01-21 22:46:40 +0530

    Time: 171 ms (21.56%), Space: 98.5 MB (17.17%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
-        map< int, int> m,ans;
-        int row = matrix.size();
-        int col = matrix[0].size();
-        
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                m[matrix[i][j]]++;
-                ans[matrix[j][i]]++;
+        int n=matrix.size();
+        set<int>s;
+        for(int i=0; i<n; i++){
+            s.clear();
+            for(int j=0; j<n; j++){
+                s.insert(matrix[i][j]);
             }
-            for(auto a : ans){
-                if(a.second>1 ){
-                    return false;
-                }
+            if(s.size()!=n){
+                return false;
             }
-            for(auto x : m){
-                if(x.second>1){
-                    return false;
-                }
+        }
+        for(int i=0; i<n; i++){
+            s.clear();
+            for(int j=0; j<n; j++){
+                s.insert(matrix[j][i]);
             }
-            for(int j=0;j<col;j++){
-                m[matrix[i][j]]=0;
-                ans[matrix[j][i]]=0;
+            if(s.size()!=n){
+                return false;
             }
         }
         return true;
