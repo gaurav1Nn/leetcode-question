0d16896d2a91b35299c9adb22c2b360746950908
gaurav1Nn <gauravnilawar26@gmail.com>
2025-05-21 09:38:53 +0530

    Time: 0 ms (100%), Space: 18.6 MB (78.87%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
+    void dozero(vector<vector<int>>& matrix ,int  i ,int  j){
+        int rows = matrix.size();
+        int columns = matrix[0].size();
+        for(int a =0;a<rows; a++){
+            matrix[a][j]=0;
+        }
+        for(int b =0;b<columns; b++){
+            matrix[i][b]=0;
+        }
+
+    }
     void setZeroes(vector<vector<int>>& matrix) {
-        int row = matrix.size();
-        int colu = matrix[0].size();
-        vector<int> r(row,0);
-        vector<int> c(colu,0);
-        for(int i=0;i<row;i++){
-            for(int j=0;j<colu;j++){
+        int rows = matrix.size();
+        vector<int> v;
+        int columns = matrix[0].size();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
                 if(matrix[i][j]==0){
-                    r[i]=1;
-                    c[j]=1;
+                    v.push_back(i);
+                    v.push_back(j);
                 }
             }
         }
-         for(int i=0;i<row;i++){
-            for(int j=0;j<colu;j++){
-                if(r[i]==1 || c[j]==1){
-                    matrix[i][j]=0;
-                }
-            }
+        for(int i=0;i<v.size();i+=2){
+            dozero(matrix , v[i],v[i+1]);
         }
-        // return matrix;
     }
 };
