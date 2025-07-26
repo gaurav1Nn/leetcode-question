c5f8ab3defb38f3a590726bcb5284cbba77b7bbf
gaurav1Nn <gauravnilawar26@gmail.com>
2025-07-26 20:19:37 +0530

    Time: 0 ms (100%), Space: 20.3 MB (48.14%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,22 +1,31 @@
 class Solution {
-
 public:
-    void dfs(int start , vector<vector<int>>& matrix , vector<bool>& visited , int n){
+    void dfs(int start , vector<vector<int>>& adj , vector<bool>& visited , int n){
         visited[start] = true;
-        for(int j=0;j<n;j++){
-            if(matrix[start][j]==1 && !visited[j]){
-                dfs(j , matrix , visited , n);
+        for(int ne: adj[start] ){
+            if(!visited[ne]){
+                dfs(ne , adj , visited , n);
             }
         }
+
     }
     int findCircleNum(vector<vector<int>>& is) {
         int n = is.size();
+
+        vector<vector<int>> adj(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && is[i][j]==1){
+                    adj[i].push_back(j);
+                }
+            }
+        }
         vector<bool> visited(n,false);
         int count = 0 ;
-        for(int i=0;i<n;i++){
+        for(int i=0; i< n;i++){
             if(!visited[i]){
                 count++;
-                dfs(i , is , visited , n);
+                dfs(i , adj , visited , n);
             }
         }
         return count;
