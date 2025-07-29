77d6fbdf08db52510e2065104247e5985a8d308e
gaurav1Nn <gauravnilawar26@gmail.com>
2025-07-29 17:37:47 +0530

    Time: 0 ms (100%), Space: 12.5 MB (67.26%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -5,14 +5,22 @@ public:
         if(n==0 || n==1) return 0;
         if(arr[0] > arr[1]) return 0; //peak is 0th index
         if(arr[n-1] > arr[n-2]) return n-1; //peak is last index
-
-        int peaki=0;
-        for(int i=1;i<n-1;i++){
-            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-                peaki = i;
-                break;
+        int low = 1;
+        int high = n-2;
+        int mid = 0 ;
+        while(low<=high){
+            mid = (low+high)/2;
+            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(arr[mid]>arr[mid-1]){
+                low = mid +1;
+            }
+            else {
+                high = mid;
             }
         }
-        return peaki;
+        return -1;
+        
     }
 };
