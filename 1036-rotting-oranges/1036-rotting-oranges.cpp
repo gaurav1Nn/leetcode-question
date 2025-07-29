00b1cdc76b0cf5d06f023811c09f4833dd2c61d9
gaurav1Nn <gauravnilawar26@gmail.com>
2025-07-29 22:33:29 +0530

    Time: 0 ms (100%), Space: 16.5 MB (97.38%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,14 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    // BFS function to simulate rotting process
     int bfs(vector<vector<int>>& grid, queue<pair<int,int>>& q, int fresh) {
         int m = grid.size();
         int n = grid[0].size();
         int minutes = -1;
-
         vector<pair<int,int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
 
         while (!q.empty()) {
@@ -17,46 +12,38 @@ public:
             while (size--) {
                 auto [r, c] = q.front();
                 q.pop();
-
                 for (auto [dr, dc] : directions) {
                     int nr = r + dr;
                     int nc = c + dc;
-
                     if (nr >= 0 && nr < m && nc >= 0 && nc < n && grid[nr][nc] == 1) {
-                        grid[nr][nc] = 2; // rot the orange
+                        grid[nr][nc] = 2;
                         fresh--;
                         q.push({nr, nc});
                     }
                 }
             }
         }
-
         return (fresh == 0) ? minutes : -1;
     }
 
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-
         queue<pair<int,int>> q;
-        int fresh = 0;
+        int fresh = 0; 
 
-        // Step 1: Collect all rotten oranges and count fresh ones
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2) {
                     q.push({i, j});
-                } else if (grid[i][j] == 1) {
+                }
+                else if (grid[i][j] == 1) {
                     fresh++;
                 }
             }
         }
 
-        if (fresh == 0) return 0; // no fresh oranges
-
-        // Step 2: Call BFS
+        if (fresh == 0) return 0;
         return bfs(grid, q, fresh);
     }
 };
-
-
