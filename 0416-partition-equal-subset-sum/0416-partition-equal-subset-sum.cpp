831eafb9811f16929318526ffc6d10209f763847
gaurav1Nn <gauravnilawar26@gmail.com>
2025-08-04 20:23:26 +0530

    Time: 143 ms (42.17%), Space: 110.5 MB (32.72%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int8_t dp[201][10001] = {[0 ... 200] = {[0 ... 10000] = -1}};  // all initialized to -1 (use dynamic vector to generalize to higher array size & possible sums)
-    bool canPartition(vector<int>& nums) {
-        int totalSum = accumulate(begin(nums), end(nums), 0);
-        if(totalSum & 1) return false;
-        return subsetSum(nums, totalSum / 2);
+    bool solve(vector<vector<int>>& dp , vector<int>& arr , int a , int i){
+        if(a==0) return true;
+        if(i ==0){
+            return arr[0]==a;
+        }
+        if(dp[i][a]!=-1) return dp[i][a];
+        bool not_take = solve(dp,arr,a,i-1);
+        bool take = false;
+        if(arr[i]<=a) take = solve(dp,arr,a-arr[i],i-1);
+        return dp[i][a] = take || not_take;
     }
-    bool subsetSum(vector<int>& nums, int sum, int i = 0) {
-        if(sum == 0) return true;
-        if(i >= size(nums) || sum < 0) return false; 
-        if(dp[i][sum] != -1) return dp[i][sum];
-        return dp[i][sum] = subsetSum(nums, sum - nums[i], i + 1) || subsetSum(nums, sum, i + 1);
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0 ; 
+        for(int x : nums){
+            sum+=x;
+        }
+        if(sum%2!=0) return false;
+        int a = sum/2;
+        vector<vector<int>> dp(n , vector<int>(a+1 , -1));
+        return solve(dp,nums,a,n-1);
     }
 };
