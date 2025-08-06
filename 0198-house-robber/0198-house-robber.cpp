5ea3fed7a49a1f4699f33dfce6857ba85f3aea4c
gaurav1Nn <gauravnilawar26@gmail.com>
2025-08-06 14:25:44 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.45%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
-    int rob(vector<int>& A) {
-        int prev2 = 0, prev = 0, cur = 0;
-        for(auto i : A) {
-            cur = max(prev, i + prev2);
-            prev2 = prev;
-            prev = cur;
-        }
-        return cur;
-    }
+  
+int solve(int idx, vector<int>& nums, vector<int>& dp) {
+    if (idx == 0) return nums[0];
+    if (idx < 0) return 0;
+    if (dp[idx] != -1) return dp[idx];
+
+    int rob = nums[idx] + solve(idx - 2, nums, dp); // Take this house
+    int skip = solve(idx - 1, nums, dp);            // Skip this house
+
+    return dp[idx] = max(rob, skip);
+}
+
+int rob(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> dp(n, -1);
+    return solve(n - 1, nums, dp);
+}
+
 };
